31d6d06275d790725ac145ebabe3de42d1e854ee
rxxuzi <75666622+rxxuzi@users.noreply.github.com>
2023-07-13 15:53:34 +0900

    generator

--- src/Sample2.ts
+++ src/Sample2.ts
@@ -1,35 +1,45 @@
-const div0 = document.createElement("div");
-div0.className = "main";
+const aiTextPrompt = document.createElement("h2");
+aiTextPrompt.id = "text-prompt";
 
+const div0 = document.createElement("div");
+
+div0.className = "prompt-list";
+div0.appendChild(aiTextPrompt);
 const div1 = document.createElement("div");
 div1.className = "prompt-list-genre";
-div0.appendChild(div1);
 
-const div2 = document.createElement("div2")
+const deleteButton = document.createElement("button");
+deleteButton.textContent = "Delete";
+deleteButton.addEventListener("click", function() {
+    aiTextPrompt.textContent = "";
+});
+
+div0.appendChild(div1);
+const div2 = document.createElement("div");
 div2.className = "description";
+div2.id = "div2";
+
 div1.appendChild(div2);
 
+
 for (let  i = 0 ; i < 10 ; i ++ ){
-
-    const btn0 = document.createElement("button");
-    btn0.textContent = `BUTTON - ${i}`;
-    btn0.className = "button";
-    btn0.appendChild(div2);
-
-}
-
-const div_ex = document.createElement("div");
-
-for (let i = 0; i < 10; i++) {
     const text = document.createElement("label");
     text.textContent = `${i} : Content`;
     const btn = document.createElement("button");
-    btn.textContent = "Submit";
-    div_ex.appendChild(text);
-    const br = document.createElement("br");
-    div_ex.appendChild(btn);
-    div_ex.appendChild(br);
 
+    // ボタンがクリックされた時のイベントリスナーを追加
+    btn.addEventListener("click", function() {
+        // ボタンのtextContentをtxt要素に追記
+        aiTextPrompt.textContent += text.textContent + " ";
+    });
+
+    btn.textContent = "Submit";
+    div2.appendChild(text);
+    const br = document.createElement("br");
+    div2.appendChild(btn);
+    div2.appendChild(br);
 }
 
-document.body.appendChild(div_ex);
+
+
+document.body.appendChild(div0);
